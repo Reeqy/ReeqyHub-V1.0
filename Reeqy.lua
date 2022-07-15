8b4e6ab066eda6a388420d11e62f5f81e5a05ece
Reeqy <109089460+Reeqy@users.noreply.github.com>
2022-07-15 15:17:54 +0100

    Update Reeqy.lua

---
+++ Reeqy.lua
@@ -0,0 +1,194 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("ReeqyHub", "Serpent")
+
+
+
+
+
+OrionLib:MakeNotification({
+	Name = "Discord",
+	Content = "discord.gg/s9C9dWhAqV",
+	Image = "rbxassetid://4483345998",
+	Time = 10
+})
+
+
+
+
+
+
+
+
+
+
+--MAIN
+local main = Window:NewTab("Universal")
+local MainSection2 = main:NewSection("Main")
+
+
+
+
+
+
+--MAIN
+local main = Window:NewTab("Jailbreak🚗")
+local MainSection = main:NewSection("Jailbreak")
+
+--Arsenal
+local Arsenal = Window:NewTab("Arsenal🔫")
+local MainSection1 = Arsenal:NewSection("Arsenal")
+
+--DaHood
+local main = Window:NewTab("DaHood❤")
+local MainSection3 = main:NewSection("DaHood")
+
+--Avatar
+local Avatar = Window:NewTab("Avatar")
+local MainSection90 = Avatar:NewSection("Avatar")
+
+
+
+
+
+local Tab = Window:NewTab("Credits✍🏻")
+local CreditsSection = Tab:NewSection("Credits")
+
+
+
+
+
+
+
+
+
+--Avatar
+MainSection90:NewButton("Free Limiteds💰🤑", "Avatar", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ForestIsPro/VisualWorks/main/FreeHats'))()
+    end)
+
+
+
+
+CreditsSection:NewButton("https://discord.gg/uZY2dzCFf2", "Discord", function()
+    print("Clicked")
+end)
+
+
+CreditsSection:NewButton("Reeqy#9499", "Discord", function()
+    print("Clicked")
+end)
+
+
+
+CreditsSection:NewButton("Credits to all script owners", "Discord", function()
+    print("Clicked")
+end)
+
+
+
+
+
+
+
+
+
+MainSection1:NewButton("Bolts Hub⚡", "Arsenal", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fusiongreg/BoltsHubV5/main/Main", true))()
+end)
+
+MainSection1:NewButton("VG Hub🏀", "Arsenal", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))()
+end)
+
+
+MainSection3:NewButton("Autofarm", "Dahood", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/racemodex/my-scripts/master/dahoodautofarm", true))()
+end)
+
+
+
+
+MainSection:NewButton("Vynixius💚", "Jailbreak", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Loader.lua"))()
+end)
+
+
+MainSection:NewButton("Auto Farm🤑", "Jailbreak", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/wawsdasdacx/ohascriptnrrewading/main/jbsaxcriptidk1"))()    end)
+
+
+
+MainSection2:NewButton("CMD-X", "Universal", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source", true))()
+end)
+
+
+
+MainSection2:NewButton("infiniteYield", "Universal", function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end)
+
+
+
+MainSection2:NewButton("AimHot-V8", "Universal", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Herrtt/AimHot-v8/master/Main.lua", true))()
+end)
+
+
+MainSection3:NewButton("SwagMode😎", "DaHood", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/lerkermer/lua-projects/master/BETA_Swagmode'))()
+end)
+
+MainSection3:NewButton("Dimag-X💎", "DaHood", function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Dimag16/DimagX_NEW/main/dimagx', true))()
+end)
+
+
+MainSection3:NewButton("Plasma🎈", "DaHood", function()
+loadstring(game:HttpGet("https://iexploit.xyz/scripts/plasma/loader"))()
+end)
+
+
+
+MainSection3:NewButton("RayX💻", "DaHood", function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/SpaceYes/Lua/Main/DaHood.Lua'))()
+end)
+
+
+MainSection3:NewButton("Arctic v9.7🔥", "DaHood", function()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/PolarWasHere/Arctic/main/Arctic"))()
+end)
+
+
+
+MainSection3:NewButton("Aim Lock🎯🔫", "DaHood", function()
+-- [[ Launcher ]] --
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/HuskyLimited/Script/main/QitkotLock.lua', true))()
+end)
+
+MainSection3:NewButton("FADED🅾🅿", "DaHood", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NighterEpic/Faded/main/YesEpic", true))()
+end)
+
+MainSection3:NewButton("Destiny🌙", "DaHood", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/compwnter/destiny/main/loader"))()
+end)
+
+
+
+
+local MainSection3 = main:NewSection("Selling GUI'S")
+
+MainSection3:NewButton("Crostide💸", "DaHood", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Crostide/cdhc/main/gui"))()
+end)
+
+
+MainSection3:NewButton("Crashing (Swagmode)", "DaHood", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lerkermer/lua-projects/master/SwagModeV002"))()
+end)
+
+
